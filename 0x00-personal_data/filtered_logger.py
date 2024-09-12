08810fea1e0ebd59678930be49f5e42d5447c8ea
Oluremi Abraham <oluremiabraham9@gmail.com>
2024-09-12 14:46:41 +0000

    Task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,3 +13,23 @@ def filter_datum(
         f"({'|'.join(fields)})=.*?{separator}",
         lambda m: f"{m.group(1)}={redaction}{separator}",
         message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Initialization method """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format for the message """
+        message = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION, message,
+                            self.SEPARATOR)

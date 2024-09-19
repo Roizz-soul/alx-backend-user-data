81c9700a158528084e5692279b42a596ffc04da0
Oluremi Abraham <oluremiabraham9@gmail.com>
2024-09-19 08:54:14 +0000

    Task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -32,3 +32,11 @@ def unauthorized():
         Raises a 401 error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """ GET /api/v1/forbidden
+        Raises a 403 error
+    """
+    abort(403)

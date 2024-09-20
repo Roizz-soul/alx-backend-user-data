4f56fa4ba907623a08c40fc286401cb23fc9e836
Oluremi Abraham <oluremiabraham9@gmail.com>
2024-09-20 09:55:16 +0000

    Task 12

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -51,7 +51,7 @@ class BasicAuth(Auth):
         if ":" not in decoded_base64_authorization_header:
             return None, None
 
-        cred = decoded_base64_authorization_header.split(":")
+        cred = decoded_base64_authorization_header.split(":", 1)
         return cred[0], cred[1]
 
     def user_object_from_credentials(

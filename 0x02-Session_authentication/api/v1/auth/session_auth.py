cb1d467915fb3311b2aabf6e5246d4072ca94cd8
Oluremi Abraham <oluremiabraham9@gmail.com>
2024-09-20 11:22:10 +0000

    Task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,3 +20,9 @@ class SessionAuth(Auth):
         sess_id = str(uuid.uuid4())
         self.user_id_by_session_id[sess_id] = user_id
         return sess_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Returns user id based on session id """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)

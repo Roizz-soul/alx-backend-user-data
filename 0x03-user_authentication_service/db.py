d5bc71e88d8f8d80969a903f6015264dd00a5a29
Oluremi Abraham <oluremiabraham9@gmail.com>
2024-09-26 16:40:02 +0000

    check 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python3
-"""DB module
+""" DB module
 """
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-
 from user import Base, User
+from typing import TypeVar
 
-
-FIELDS = ['id', 'email', 'hashed_password', 'session_id',
-          'reset_token']
+VALID_FIELDS = ['id', 'email', 'hashed_password', 'session_id',
+                'reset_token']
 
 
 class DB:
-    """DB class
+    """
+    DB class.
     """
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance
+    def __init__(self):
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Constructor.
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
+    def _session(self):
+        """
+        _session.
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -37,32 +39,37 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Adds a nnew user """
-        new_user = User(email=email, hashed_password=hashed_password)
+        """
+        add_user.
+        """
+        if not email or not hashed_password:
+            return
+        user = User(email=email, hashed_password=hashed_password)
         session = self._session
-        session.add(new_user)
+        session.add(user)
         session.commit()
-        return new_user
+        return user
 
     def find_user_by(self, **kwargs) -> User:
-        """ Finds a user """
-        if not kwargs or any(x not in FIELDS for x in kwargs):
+        """
+        find_user_by.
+        """
+        if not kwargs or any(x not in VALID_FIELDS for x in kwargs):
             raise InvalidRequestError
         session = self._session
         try:
-            user = session.query(User).filter_by(**kwargs).one()
-            if user is None:
-                raise NoResultFound
-            return user
+            return session.query(User).filter_by(**kwargs).one()
         except Exception:
             raise NoResultFound
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """ update user data """
+        """
+        update_user.
+        """
         session = self._session
         user = self.find_user_by(id=user_id)
         for k, v in kwargs.items():
-            if k not in FIELDS:
+            if k not in VALID_FIELDS:
                 raise ValueError
             setattr(user, k, v)
         session.commit()
